e13db6cbf7d8d3e37a946c50f01dbcbf41c28ec4
Hardik Chauhan <hardik.chauhan@bacancytechnology.com>
2019-09-26 16:04:32 +0530

    Development changes - 26092019

--- trackonline.php
+++ trackonline.php
@@ -9,7 +9,7 @@ if (!empty($_GET['email'])){
     $updated_at = date('Y-m-d H:i:s');
     mysqli_query($con,"UPDATE `emails` SET `read_status` = '1',`updated_at` = '".$updated_at."' WHERE `uniq_id` = '".$uniq_id."'");
     $fh = fopen("email.txt", "a+"); //file handler
-    $string = $email." opened the mail with subject: ".$sub." on %".$date."% with mailId: ".$uniq_id."\n";
+    $string = $email." opened the mail with subject: ".$sub." on %".$updated_at."% with mailId: ".$uniq_id."\n";
     fwrite($fh, $string);
     fclose($fh);
     //All done, get out!
